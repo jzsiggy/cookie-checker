18e8c91f94946f3da642d3fcaee0c8a0c8de8fe8
Joao Zsigmond <jzsiggy@gmail.com>
2023-06-05 23:47:16 -0300

    adding everything in tables

--- popup.js
+++ popup.js
@@ -1,38 +1,66 @@
-// send a message to the background script
+// Function to create table from JSON data
+function createTable(jsonData, tableId) {
+    let table = document.getElementById(tableId);
+
+    // Determine if jsonData contains objects or strings
+    let isObject = jsonData[0] !== null && typeof jsonData[0] === 'object';
+
+    let data = isObject ? Object.keys(jsonData[0]) : ["URL"];
+    let tableHead = document.createElement('thead');
+    let tableBody = document.createElement('tbody');
+    let headRow = document.createElement('tr');
+
+    // Create table header row
+    data.forEach((key) => {
+        let th = document.createElement('th');
+        let text = document.createTextNode(key);
+        th.appendChild(text);
+        headRow.appendChild(th);
+    });
+    tableHead.appendChild(headRow);
+
+    // Create table body rows
+    jsonData.forEach((element) => {
+        let tr = document.createElement('tr');
+        data.forEach((key) => {
+            let td = document.createElement('td');
+            let text = document.createTextNode(isObject ? element[key] || '' : element);
+            td.appendChild(text);
+            tr.appendChild(td);
+        });
+        tableBody.appendChild(tr);
+    });
+
+    table.appendChild(tableHead);
+    table.appendChild(tableBody);
+}
+
+
+// Send a message to the background script
 browser.runtime.sendMessage({greeting: "hello"}, function(response) {
     // handle the cookie data
-    let cookieData = response.data;
-    let initialCookieData = cookieData.slice(0, 10);
-    document.getElementById('cookie-content').textContent = JSON.stringify(initialCookieData, null, 2);
+    createTable(response.data, 'cookie-table');
 
-    // if there are more cookies, show the "Show more" button
-    if (cookieData.length > 10) {
-        document.getElementById('showMoreCookies').style.display = 'block';
-    }
-
-    document.getElementById('showMoreCookies').addEventListener('click', function() {
-        document.getElementById('cookie-content').textContent = JSON.stringify(cookieData, null, 2);
-        this.style.display = 'none';
-    });
-    
     // handle the local storage data
     let localStorageData = JSON.parse(response.localStorageData);
-    document.getElementById('storage-content').textContent = JSON.stringify(localStorageData, null, 2);
-    
+    let formattedLocalStorageData = Object.keys(localStorageData).map((key) => ({
+        key: key,
+        value: localStorageData[key]
+    }));
+    createTable(formattedLocalStorageData, 'storage-table');
+
     // handle the third-party connections
-    let thirdPartyConnections = response.thirdPartyConnections;
-    document.getElementById('thirdparty-content').textContent = JSON.stringify(thirdPartyConnections, null, 2);
-    
+    createTable(response.thirdPartyConnections, 'thirdparty-table');
+
     // handle the potential cookie syncs
-    let potentialCookieSyncs = response.potentialCookieSyncs;
-    document.getElementById('cookiesync-content').textContent = JSON.stringify(potentialCookieSyncs, null, 2);
+    createTable(response.potentialCookieSyncs, 'cookiesync-table');
 
     // create a summary
     let summary = `
-        Cookies found: ${cookieData.length}<br/>
-        Local Storage items found: ${Object.keys(localStorageData).length}<br/>
-        Third Party connections: ${thirdPartyConnections.length}<br/>
-        Potential Cookie Synchronizations: ${potentialCookieSyncs.length}
+    Cookies found: ${response.data.length}<br/>
+    Local Storage items found: ${Object.keys(localStorageData).length}<br/>
+    Third Party connections: ${response.thirdPartyConnections.length}<br/>
+    Potential Cookie Synchronizations: ${response.potentialCookieSyncs.length}
     `;
     document.getElementById('summary').innerHTML = summary;
 });
